e540e8b92638ee6b5a486975b31b4be9b2387c2a
willswats <williamstuwatson@gmail.com>
2022-05-30 10:56:55 +0100

    Remove dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 
-import DropDown from './components/DropDown/DropDown';
 import Calculator from './components/Calculator/Calculator';
 
 const App = () => {
   return (
     <div className="app">
-      {/* <DropDown /> */}
       <Calculator />
     </div>
   );
