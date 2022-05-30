e6bb6de559c8a718e69876ae6f9a26eee3341212
willswats <williamstuwatson@gmail.com>
2022-05-30 16:49:49 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Calculator from './components/Calculator/Calculator';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="app">
       <Calculator />
+      <Footer />
     </div>
   );
 };
