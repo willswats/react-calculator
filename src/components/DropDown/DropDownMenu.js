2313dd120bbbde6e398d951dcf23dc60d0ea939d
willswats <williamstuwatson@gmail.com>
2022-06-01 11:11:23 +0100

    Change background and dropdown

--- src/components/DropDown/DropDownMenu.js
+++ src/components/DropDown/DropDownMenu.js
@@ -7,16 +7,14 @@ const DropDownMenu = ({ dropDown, setDropDown }) => {
         onClick={() => setDropDown(!dropDown)}
         className={classes['dropdown-menu__btn']}
       ></button>
-      <div>
-        <h1 className={classes['dropdown-menu__title']}>Keyboard shortcuts</h1>
-        <li className={classes['dropdown-menu__list']}>
-          <ol>ESC - AC</ol>
-          <ol>Backspace - DEL</ol>
-          <ol>Arrow right - History</ol>
-          <ol>Arrow left - Calculator</ol>
-        </li>
-        <p>The others correspond to the exact keys on the keyboard.</p>
-      </div>
+      <h1 className={classes['dropdown-menu__title']}>Keyboard shortcuts</h1>
+      <li className={classes['dropdown-menu__list']}>
+        <ol>AC - ESC</ol>
+        <ol>DEL - Backspace</ol>
+        <ol>History - Arrow right</ol>
+        <ol>Calculator - Arrow left</ol>
+      </li>
+      <p>The others correspond to the exact keys on the keyboard.</p>
     </div>
   );
 };
